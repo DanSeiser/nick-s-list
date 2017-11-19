64b5ed2ec4e30a0ccf3ba11cbe085f09b53d0b25
Kate Hayes <kate.beaver@gmail.com>
2017-11-19 11:50:12 -0600

    React App files, No Yeoman.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Navbar from './Navbar';
+import Jumbotron from './Jumbotron';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => (
+  <div className="container">
+    <Navbar />
+    <Jumbotron />
+  </div>
+);
+
+
+export default App;
